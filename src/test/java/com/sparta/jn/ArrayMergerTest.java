05ee3d817a568b0eb789d3c8a2c92cb31861b043
Jimmy Ng <ngkwoklung@gmail.com>
2022-08-04 11:08:20 +0100

    "Junit Template"

---
+++ src/test/java/com/sparta/jn/ArrayMergerTest.java
@@ -0,0 +1,11 @@
+package com.sparta.jn;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ArrayMergerTest {
+
+    @Test
+    @DisplayName("Should merge two sorted int arrays in one sorted array")
+    void ShouldMergeTwo
+}
