6f4b6beb69a49c7874e673c542904c20bbff07e8
Jimmy <ngkwoklung@gmail.com>
2022-08-04 18:46:56 +0100

    Cleaned up reds

--- src/test/java/com/sparta/jn/AppTest.java
+++ src/test/java/com/sparta/jn/AppTest.java
@@ -1,18 +1,9 @@
 package com.sparta.jn;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
+
 }
